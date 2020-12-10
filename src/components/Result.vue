66479d16cd8ef751a443da8faa44edc8d515e348
Andrenoj11 <andrenojuanda94@gmail.com>
2020-12-11 01:12:55 +0700

    add main page and countdown

---
+++ src/components/Result.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <button
+      type="button"
+      class="btn btn-success"
+      @click="countDownTimer"
+      v-if="countDown > 0"
+    >
+      Play Game
+    </button>
+    <button
+      v-else
+      type="button"
+      class="btn btn-secondary ml-2"
+      @click="resetTimer"
+    >
+      Reset
+    </button>
+    <h2 class="mt-5 mb-5" v-if="countDown > 0">{{ countDown }}s</h2>
+    <h2 class="mt-5 mb-5" v-else>Its a {{ color }}</h2>
+    <!-- <div class="card text-black bg-light mb-3 ml-5 mr-5 mt-5" v-if="countDown === 0" >
+      <div class="card-header"><strong>RESULT</strong></div>
+      <div class="card-body">
+        <h5 class="card-title">{{ color }}</h5>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      countDown: 2,
+      colors: ['Red', 'Black', 'Green'],
+      color: ''
+    }
+  },
+  methods: {
+    countDownTimer () {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1
+          this.countDownTimer()
+        }, 1000)
+      }
+      const rand = Math.floor(Math.random() * 3)
+      this.color = this.colors[rand]
+    },
+    resetTimer () {
+      this.countDown = 2
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mainpage">
+    <div class="" >
+      <div class="row" style="height: 90vh;">
+        <div class="col" >
+          <h4>Hi player</h4>
+          <TotalPrizeMoney style="margin-top: 90px;"></TotalPrizeMoney>
+        </div>
+        <div class="col-6">
+          <Result></Result>
+        </div>
+        <div class="col">One of three columns</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TotalPrizeMoney from '../components/TotalPrizeMoney'
+// import HelloWorld from '../components/HelloWorld'
+import Result from '../components/Result'
+
+export default {
+  name: 'MainPage',
+  components: {
+    TotalPrizeMoney,
+    Result
+  }
+}
+</script>
+
+<style>
+
+</style>

34ed9278ad1d5d0a8da7932dafe605df2d3bd097
jarooda <jaluwibowoaji@gmail.com>
2020-12-11 01:08:24 +0700

    client To server 1. add logic socket io to Home.vue 2. add state, mutations in index.js in store

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,39 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    history: [],
+    totalPrizeMoney: 0,
+    yourMoney: 0,
+    probability: {},
+    result: {}
   },
   mutations: {
+    SOCKET_init (state, payload) {
+      state.history = payload.history
+      state.totalPrizeMoney = payload.totalPrizeMoney
+      state.probability = payload.probability
+      state.yourMoney = 100000
+    },
+    SOCKET_messageWrong (state, payload) {
+      state.history.push({ message: payload.message })
+      state.totalPrizeMoney += +payload.bet
+    },
+    SOCKET_resultWrong (state, payload) {
+      state.yourMoney -= +payload.bet
+    },
+    SOCKET_resultRight (state, payload) {
+      state.yourMoney += +payload.gain
+      state.totalPrizeMoney = 50000
+    },
+    SOCKET_messageRight (state, payload) {
+      state.history.push({ message: payload.message })
+    },
+    SOCKET_message (state, payload) {
+      state.history.push({ message: payload.message })
+    },
+    SOCKET_result (state, payload) {
+      state.result = payload.result
+    }
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1>Total Money You can get</h1>
+      <h1>Rp. {{ totalPrizeMoney }},-</h1>
+    </div>
+    <div>
+      <h1>Last Result = {{ result.num }} {{ result.color }}</h1>
+    </div>
+    <div>
+      <h4>Name</h4>
+      <input type="text" placeholder="Your Name" v-model="name"><br>
+      <h4>Your Money Rp. {{ yourMoney }},-</h4>
+      <div v-if="yourMoney == 0">
+        <h1>Uangmu habis, sana cari Pinjol</h1>
+      </div>
+      <div v-else>
+        <form @submit.prevent="betting">
+        <input type="number" placeholder="Your Bet" min="1" :max=yourMoney style="width:200px" v-model="bet"><br>
+        <select v-model="num">
+          <option
+          v-for="prob in probability" :key="prob.num"
+          >
+          {{ prob.num}}
+          </option>
+        </select>
+        <button type="submit">Bet</button>
+      </form>
+      </div>
+    </div>
+    <div>
+        <h1>HISTORY</h1>
+        <p
+        v-for="(msg, id) in history" :key="id"
+        >{{ msg.message }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name: '',
+      bet: '',
+      num: ''
+    }
+  },
+  computed: {
+    history () {
+      return this.$store.state.history
+    },
+    totalPrizeMoney () {
+      return this.$store.state.totalPrizeMoney
+    },
+    yourMoney () {
+      return this.$store.state.yourMoney
+    },
+    probability () {
+      return this.$store.state.probability
+    },
+    result () {
+      return this.$store.state.result
+    }
+  },
+  methods: {
+    betting () {
+      this.$socket.emit('betting', { name: this.name, bet: this.bet, num: this.num })
+    }
+  },
+  sockets: {
+    connect () {
+      console.log('connected to socket.io')
+    }
   }
 }
 </script>
